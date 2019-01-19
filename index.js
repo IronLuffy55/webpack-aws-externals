f7779ed7f27c6255cb104dd1b1111df782b57c60
IronLuffy55 <interianoe55@gmail.com>
2019-01-19 18:38:46 -0500

    Removed commented out lines/unused options

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var utils = require("./utils");
 var awsPackages = JSON.parse(require("./aws_packages")) || [];
-console.log("Exclude>", awsPackages);
 var scopedModuleRegex = new RegExp(
   "@[a-zA-Z0-9][\\w-.]+/[a-zA-Z0-9][\\w-.]+([a-zA-Z0-9./]+)?",
   "g"
@@ -25,46 +24,22 @@ function getModuleName(request, includeAbsolutePaths) {
 module.exports = function nodeExternals(options) {
   options = options || {};
   var whitelist = [].concat(options.whitelist || []);
-  console.log("whitelist>", whitelist);
-  var binaryDirs = [].concat(options.binaryDirs || [".bin"]);
   var importType = options.importType || "commonjs";
-  console.log("importType>", importType);
-  var modulesDir = options.modulesDir || "node_modules";
-  var modulesFromFile = !!options.modulesFromFile;
   var includeAbsolutePaths = !!options.includeAbsolutePaths;
-
-  // helper function
-  function isNotBinary(x) {
-    return !utils.contains(binaryDirs, x);
-  }
-
-  // create the node modules list
-  var nodeModules = awsPackages;
-  //   modulesFromFile
-  //     ? utils.readFromPackageJson(options.modulesFromFile)
-  //     : utils.readDir(modulesDir).filter(isNotBinary);
-
-  //   console.log("nodeModules>", nodeModules);
   // return an externals function
-  return function(context, request, callback) {
+  return function(_, request, callback) {
     var moduleName = getModuleName(request, includeAbsolutePaths);
     if (
-      utils.contains(nodeModules, moduleName) &&
+      utils.contains(awsPackages, moduleName) &&
       !utils.containsPattern(whitelist, request)
     ) {
-      //DO NOT TRANSPILE moduleName
-      console.log("request>", request);
-
       if (typeof importType === "function") {
-        console.log("moduleName: cb1>", JSON.stringify(moduleName));
         return callback(null, importType(request));
       }
       // mark this module as external
       // https://webpack.js.org/configuration/externals/
-      console.log("moduleName: cb2>", JSON.stringify(moduleName));
       return callback(null, importType + " " + request);
     }
-    // console.log("moduleName: cb3>", JSON.stringify(moduleName));
     callback();
   };
 };
